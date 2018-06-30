9aaf75f7075658e01152a188362aac8004875410
Joel Wall <s3548001@student.rmit.edu.au>
2018-06-30 06:40:19 -0700

    Added options table view and model structure
    Also centred some of the images

---
+++ RepCheck/Model/Model.swift
@@ -0,0 +1,22 @@
+//
+//  Model.swift
+//  RepCheck
+//
+//  Created by Joel Wall on 30/6/18.
+//  Copyright © 2018 Joel Wall. All rights reserved.
+//
+
+import Foundation
+
+//holds all out model data once instansiated
+class Model{
+    //holds our options portion of model data
+    let options : Options
+    //add other model data here
+    
+    //initialise the model
+    init(){
+        ///instansiate the options
+        self.options = Options()
+    }
+}
---
+++ RepCheck/Model/Options.swift
@@ -0,0 +1,23 @@
+//
+//  OptionsList.swift
+//  RepCheck
+//
+//  Created by Joel Wall on 30/6/18.
+//  Copyright © 2018 Joel Wall. All rights reserved.
+//
+
+import Foundation
+
+//options and option functionality for the options view!
+struct Options {
+    
+    //Has our list of Options as strings that will be passed to the controller and displayed in view.
+    var optionsList = [String]()
+    let sections = 1
+        
+    
+    init(){
+        self.optionsList.append("Clear History")
+        self.optionsList.append("About Repcheck")
+    }
+}
