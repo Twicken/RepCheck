24272e0c08d714926633152c021c59dc05d4e0b1
Joel Wall <s3548001@student.rmit.edu.au>
2018-07-04 23:47:53 -0700

    Added 3 steps and functioning navigation
    yay

---
+++ RepCheck/New Group3/Step1ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  RepCheck
+//
+//  Created by Joel Wall on 30/6/18.
+//  Copyright © 2018 Joel Wall. All rights reserved.
+//
+
+import UIKit
+
+class Step1ViewController: UIViewController {
+    
+    @IBOutlet weak var nextButton: UIButton!
+    //may need this code later. It's for programatically going to step 2 rather than by storyboard.
+    /*
+    @IBAction func triggered(_ sender: Any) {
+        let viewControllers: [UIViewController] = self.navigationController!.viewControllers
+        for aViewController in viewControllers {
+            if aViewController is Step2ViewController {
+                
+                self.show(aViewController, sender:self)
+                
+                break
+            }else{
+                let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+                let nextViewController = storyBoard.instantiateViewController(withIdentifier: "step2")
+                (nextViewController as! Step2ViewController).pastViewController=self
+                self.show(nextViewController, sender:self)
+            }
+        }
+    }*/
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+  //  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+
+  //  }
+
+
+}
---
+++ RepCheck/New Group3/Step3ViewController.swift
@@ -0,0 +1,35 @@
+
+import UIKit
+
+class Step3ViewController: UIViewController {
+    var pastViewController: UIViewController?
+    
+    @IBOutlet weak var backButton: UIButton!
+    @IBAction func triggered(_ sender: Any) {
+        
+        
+        self.navigationController?.popViewController(animated: true)
+    }
+    // Do any additional setup after loading the view.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
