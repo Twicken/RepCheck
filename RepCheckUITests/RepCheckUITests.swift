816258dd91bb1f93f203105111c712098b88e9f8
JosephGalati <s3538715@student.rmit.edu.au>
2018-07-13 17:52:10 +1000

    UI Testing Scene 1
    Has UI Testing for Scene 1

--- RepCheckUITests/RepCheckUITests.swift
+++ RepCheckUITests/RepCheckUITests.swift
@@ -28,9 +28,63 @@ class RepCheckUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testElementsScene1() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        // Get a reference to the app
+        let app = XCUIApplication()
+        
+        
+        // use the identifier of the textboxes for to establish testing
+        let firstnamebox=app.textFields["firstnamebox"]
+        let middlenamebox=app.textFields["middlenamebox"]
+        let surnamebox=app.textFields["surnamebox"]
+
+        // Below line Used in identifying elements
+        // print(app.debugDescription)
+        
+        // Test that the first text box (first name) is found and accepts user inputs
+        firstnamebox.tap()
+        firstnamebox.typeText("Bob")
+        
+        // Test that the second text box (middle name) is found and accepts user inputs
+        middlenamebox.tap()
+        middlenamebox.typeText("Jones")
+        
+        // Test that the third text box (surname) is found and accepts user inputs
+        surnamebox.tap()
+        surnamebox.typeText("Miller")
+        
+        // Test of the picker wheel
+        let pickerWheel = app.pickers["countrypicker"].children(matching: .pickerWheel).element
+        pickerWheel.swipeUp()
+        pickerWheel.tap()
+        
+        // Test that there are 7 Static texts on the screen
+        
+        XCTAssertEqual(app.staticTexts.count, 7)
+        
+        // Test that there are 8 buttons on the screen
+        XCTAssertEqual(app.buttons.count,8)
+    
+        // Test that there are only 1 image on the screen
+        XCTAssertEqual(app.images.count,1)
+        
+        // Test the Reset button
+        app.buttons["Reset"].tap()
+        
+                // Test the Next button
+        app.buttons["Next →"].tap()
+        
+        
+        
+        
+        
+        
+        
     }
+    
+    
     
 }
