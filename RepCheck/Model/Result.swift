f1d012fd2b6f1ce569894a1fed0ae7baa3c4bfcb
Joel Wall <s3548001@student.rmit.edu.au>
2018-07-25 00:13:38 -0700

    Added CoreData data model and modified the appdelegate

--- RepCheck/Model/Result.swift
+++ RepCheck/Model/Result.swift
@@ -10,6 +10,7 @@ class Result{
     //stores a list of results. will be gotten from api later on
     var resultList = ["https://www.facebook.com/public/Joel-Wall","https://www.linkedin.com/pub/dir/Joel/Wall","https://www.astrolighting.com/products/7157-joel-wall", "https://www.geelongadvertiser.com.au/.../joel.../f3001127edca1ccc6f8875bea6503f2f"]
     var score = 0//for storing repscore in future
+    
     init(){}
     
 
